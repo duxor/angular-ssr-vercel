7e22841edab15f53c05b320d4d28b633cad4c16a
Dusan Perisic <dullle@live.com>
2025-05-15 09:02:25 +0200

    test update

--- api/index.js
+++ api/index.js
@@ -9,42 +9,32 @@ const fs = require('fs');
 const serverDistPath = path.join(process.cwd(), 'dist/angular-ssr-vercel/server/server.mjs');
 
 // Export a handler function for Vercel
-module.exports = async (req, res) => {
-  try {
-    fs.readdir('./', (err, files) => {
-      const fileList = [];
-      files.forEach(file => {
-        fileList.push(file);
-      });
-      console.log('Current directory files:', fileList);
-    });
+// module.exports = async (req, res) => {
+//   try {
+//     // Check if the server file exists
+//     if (!fs.existsSync(serverDistPath)) {
+//       console.error(`Server file not found at: ${serverDistPath}`);
+//       return res.status(500).send(`[1] Server file not found`);
+//     }
+//
+//     // Dynamically import the server module (it's an ESM module)
+//     const serverModule = await import(serverDistPath);
+//
+//     // Get the app from the server module
+//     const app = serverModule.app;
+//
+//     // If app is not available, return an error
+//     if (!app) {
+//       console.error('Server app not found in the imported module');
+//       return res.status(500).send(`[2] Server app not found ${serverDistPath}`);
+//     }
+//
+//     // Use the app to handle the request
+//     return app(req, res);
+//   } catch (error) {
+//     console.error('Error handling request:', error);
+//     return res.status(500).send('Internal Server Error');
+//   }
+// };
 
-    // Check if the server file exists
-    if (!fs.existsSync(serverDistPath)) {
-      console.error(`Server file not found at: ${serverDistPath}`);
-      return res.status(500).send(`[1] Server file not found`);
-    }
-
-    // Dynamically import the server module (it's an ESM module)
-    const serverModule = await import(serverDistPath);
-
-    // Get the app from the server module
-    const app = serverModule.app;
-
-    // If app is not available, return an error
-    if (!app) {
-      console.error('Server app not found in the imported module');
-      console.log('Server module:', serverDistPath);
-      console.log('Server dir:', serverDistPath);
-      return res.status(500).send(`[2] Server app not found ${serverDistPath}`);
-    }
-
-    // Use the app to handle the request
-    return app(req, res);
-  } catch (error) {
-    console.error('Error handling request:', error);
-    return res.status(500).send('Internal Server Error');
-  }
-};
-
-// export default import('server.mjs').then(module => module.app());
+export default import(serverDistPath).then(module => module.app);
